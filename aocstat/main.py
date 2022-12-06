daab28d1a98f89540f5b493eb63ec6bdc702fb87
Hector Brown <hectorjbrown@protonmail.com>
2022-12-06 17:40:56 +0000

    begin cli design for lb

--- aocstat/main.py
+++ aocstat/main.py
@@ -7,11 +7,48 @@ def start(args=sys.argv):
     parser = argparse.ArgumentParser(
         description="Interact with Advent of Code from your terminal."
     )
-    args = parser.parse_args(args)
+    parser.add_argument("subcommand", choices=["lb", "purge"])
+    parser.add_argument("subcommand_args", nargs=argparse.REMAINDER)
+    args = vars(parser.parse_args(args))
+    if args["subcommand"] == "lb":
+        lb(args=args["subcommand_args"])
 
-    # display help if empty
-    if len(args.__dict__) == 0:
-        parser.print_help()
+
+def lb(args=sys.argv):
+    parser = argparse.ArgumentParser(
+        description="Access and modify public and private Advent of Code leaderboards."
+    )
+    parser.add_argument(
+        "-y",
+        "--year",
+        nargs=1,
+        metavar="YEAR",
+        help="Specify a year other than the most recent event.",
+    )
+    priv_glob = parser.add_mutually_exclusive_group()
+    priv_glob.add_argument(
+        "--id",
+        metavar="ID",
+        help="Specify a private leaderboard id other than your own. Cannot be used with '-g, --global'",
+    )
+    priv_glob.add_argument(
+        "-g",
+        "--global",
+        default=False,
+        nargs="?",
+        metavar="DAY",
+        help="View the global leaderboard. optionally include a day number in the range [1..25]. Cannot be used with '--id'",
+    )
+    parser.add_argument(
+        "-f",
+        "--force",
+        default=False,
+        action="store_true",
+        help="Force update leaderboard, even if within the cache ttl. "
+        + "Please use responsibly (preferably not at all) and be considerate of others, especially in December!",
+    )
+    args = vars(parser.parse_args(args))
+    print(args)
 
 
 if __name__ == "__main__":
