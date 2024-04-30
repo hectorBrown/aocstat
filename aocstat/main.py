ac7e4f3bbc6983518a7541b6d03db5f3653c8269
Hector Brown <hectorjbrown@protonmail.com>
2024-04-30 19:52:23 +0100

    add handling for no private lbs + default priv lb

--- aocstat/main.py
+++ aocstat/main.py
@@ -43,24 +43,6 @@ def lb(args=sys.argv[1:]):
         type=int,
         help="Specify a year other than the most recent event.",
     )
-    priv_glob = parser.add_mutually_exclusive_group()
-
-    def lb_id_type(arg):
-        lb_ids = api.get_lb_ids()
-        if arg in [str(x) for x in lb_ids]:
-            return int(arg)
-        else:
-            raise argparse.ArgumentTypeError(
-                f"Invalid leaderboard id '{arg}'. Must be one of {lb_ids}."
-            )
-
-    priv_glob.add_argument(
-        "--id",
-        metavar="ID",
-        type=lb_id_type,
-        help="Specify a private leaderboard id other than your own. Cannot be used with '-g, --global'",
-        # TODO: set default to your lb if exists otherwise alternate
-    )
 
     def glob_lb_day_type(arg):
         if re.match(r"^(0?[1-9]|1[0-9]|2[0-5]):[12]$", arg):
@@ -70,15 +52,44 @@ def lb(args=sys.argv[1:]):
                 "day:part must be in the form 'd:p' where d is the day and p is the part."
             )
 
-    priv_glob.add_argument(
-        "-g",
-        "--global",
-        default=False,
-        nargs="?",
-        metavar="DAY",
-        type=glob_lb_day_type,
-        help="View the global leaderboard. optionally include a day number in the form ('[1..25]:[1,2]') where the number after the colon denotes which part to view. Cannot be used with '--id'",
-    )
+    if api.get_lb_ids():
+        priv_glob = parser.add_mutually_exclusive_group()
+
+        def lb_id_type(arg):
+            lb_ids = api.get_lb_ids()
+            if arg in [str(x) for x in lb_ids]:
+                return int(arg)
+            else:
+                raise argparse.ArgumentTypeError(
+                    f"Invalid leaderboard id '{arg}'. Must be one of {lb_ids}."
+                )
+
+        priv_glob.add_argument(
+            "--id",
+            metavar="ID",
+            type=lb_id_type,
+            help="Specify a private leaderboard id. Cannot be used with '-g, --global'",
+            default=api.get_lb_ids()[-1],
+        )
+
+        priv_glob.add_argument(
+            "-g",
+            "--global",
+            default=False,
+            nargs="?",
+            metavar="DAY",
+            type=glob_lb_day_type,
+            help="View the global leaderboard. optionally include a day number in the form ('[1..25]:[1,2]') where the number after the colon denotes which part to view. Cannot be used with '--id'",
+        )
+    else:
+        parser.add_argument(
+            "-d",
+            "--day",
+            default=None,
+            type=glob_lb_day_type,
+            help="A day number in the form ('[1..25]:[1,2]') where the number after the colon denotes which part to view.",
+        )
+
     parser.add_argument(
         "-f",
         "--force",
@@ -88,14 +99,18 @@ def lb(args=sys.argv[1:]):
         + "Please use responsibly (preferably not at all) and be considerate of others, especially in December!",
     )
     args = vars(parser.parse_args(args))
-    if args["global"] is False:
-        # TODO: read config file to get custom ttl
-        _lb = api.get_priv_lb(
-            id=args["id"], yr=args["year"], force_update=args["force"]
-        )
-        print(fmt.format_priv_lb(_lb))
+    if api.get_lb_ids():
+        if args["global"] is False:
+            # TODO: read config file to get custom ttl
+            _lb = api.get_priv_lb(
+                id=args["id"], yr=args["year"], force_update=args["force"]
+            )
+            print(fmt.format_priv_lb(_lb))
+        else:
+            _lb = api.get_glob_lb(yr=args["year"], day=args["global"])
+            print(fmt.format_glob_lb(_lb))
     else:
-        _lb = api.get_glob_lb(yr=args["year"], day=args["global"])
+        _lb = api.get_glob_lb(yr=args["year"], day=args["day"])
         print(fmt.format_glob_lb(_lb))
 
 
